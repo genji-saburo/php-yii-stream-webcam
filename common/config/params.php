0bb20b1b334df63b31fe47b28ea54d273df0a770
Genji Saburo <gen.sa@aol.com>
2017-06-23 05:38:38 +0300

    Mail DB Credentials are changed from source code

--- common/config/params.php
+++ common/config/params.php
@@ -8,8 +8,8 @@ return [
     'twillio.alarm_message' => 'Patroleum: Alarm has been iniciated for your property.',
     'API_PIN_EXPIRE_SECONDS' => 10,
     'ALERT_MAIL_DB' => 'dbmail',
-    'ALERT_MAIL_DB_USERNAME' => 'master',
-    'ALERT_MAIL_DB_PASSWORD' => 'PatrPass0',
+    'ALERT_MAIL_DB_USERNAME' => 'root',
+    'ALERT_MAIL_DB_PASSWORD' => '1b2dfwefewrffer29fweF27252rjPG',
     'WS_DOMAIN' => 'platform.patroleum.net',
     'WS_PORT' => '8080'
 ];
